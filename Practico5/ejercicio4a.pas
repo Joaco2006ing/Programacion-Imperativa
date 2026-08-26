1921d44201ef770419a9a885335fd21a5a3ff394
Joaco2006ing <joacomar2015@gmail.com>
2026-08-26 18:54:02 -0300

    para que quede mas lindo

--- Practico5/ejercicio4a.pas
+++ Practico5/ejercicio4a.pas
@@ -8,13 +8,13 @@ suma := 0;
 contador:=0;
 prom:=0;
 read (valor);
-while valor > 0 do
-begin
-suma:= suma +valor;
-contador:=contador+1;
-read(valor)
+  while valor > 0 do
+    begin
+        suma:= suma +valor;
+        contador:=contador+1;
+        read(valor)
 end;
-prom:= suma div contador;
-writeln('La suma total es ', suma);
-writeln('El promedio total es ', prom);
+  prom:= suma div contador;
+    writeln('La suma total es ', suma);
+    writeln('El promedio total es ', prom);
 end.
---
+++ Practico5/ejercicio4b.pas
@@ -0,0 +1,2 @@
+{Escriba un programa en Pascal que determine el n´umero m´as grande y el m´as peque˜no
+de todos los enteros positivos le´ıdos. Exhiba ambos resultados en la salida est´andar.}
