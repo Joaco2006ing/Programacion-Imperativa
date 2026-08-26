b45e829fd878a04c95a4995bfd5246115d6fd226
Joaco2006ing <joacomar2015@gmail.com>
2026-08-26 19:02:21 -0300

    Ejercicio4b

--- Practico5/ejercicio4b.pas
+++ Practico5/ejercicio4b.pas
@@ -1,2 +1,19 @@
 {Escriba un programa en Pascal que determine el n´umero m´as grande y el m´as peque˜no
 de todos los enteros positivos le´ıdos. Exhiba ambos resultados en la salida est´andar.}
+program ejercicio4b;
+var maximo, minimo, numero: integer;
+begin
+  write('Ingrese una secuencia de numeros (termina con -1): ');
+  read(numero);
+  maximo := numero;
+  minimo := numero;
+  repeat
+    if numero > maximo then 
+      maximo := numero
+    else if numero < minimo then 
+      minimo := numero;
+    read(numero);
+  until numero = -1;
+  writeln('El mayor entero ingresado es: ', maximo);
+  writeln('El menor entero ingresado es: ', minimo);
+end.
