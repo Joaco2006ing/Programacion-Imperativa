5e5e40d208b8ae98ab8c50c674f1d63dc73e8d6b
Joaco2006ing <joacomar2015@gmail.com>
2026-09-01 00:09:30 -0300

    Ejercicio 5

---
+++ Practico7/ejercicio5.pas
@@ -0,0 +1,29 @@
+{
+  Ejercicio 5
+  Consigna:
+  Implemente el procedimiento:
+    procedure rectangulo (altura, ancho : integer; c: char);
+  que dibuja en la salida estándar un rectángulo con lados altura y ancho 
+  "pintado" con el carácter c.
+  
+  Ejemplo: al invocar rectangulo(4, 10, '*') se debe desplegar:
+  **********
+  **********
+  **********
+  **********
+}
+program llamada;
+procedure rectangulo(altura, ancho: integer; c: char);
+var
+  i, j: integer;
+begin
+  for i := 1 to altura do
+  begin
+    for j := 1 to ancho do
+      write(c);
+    writeln; { Baja a la siguiente fila }
+  end;
+end;
+begin
+  rectangulo(3,4,'*');
+end.
