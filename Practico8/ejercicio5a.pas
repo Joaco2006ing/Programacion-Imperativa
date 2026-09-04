461be57aa20acb6125ecb69ccdd551a38cd6a149
Joaco2006ing <joacomar2015@gmail.com>
2026-09-03 23:10:14 -0300

    Practico 8: ejercicios iniciales

---
+++ Practico8/ejercicio5a.pas
@@ -0,0 +1,39 @@
+{
+====================================================================
+EJERCICIO 5 (a) - ALCANCE DE IDENTIFICADORES
+====================================================================
+
+1. DENTRO DEL PROGRAMA x:
+--------------------------------------------------------------------
+- a : Constante     (declarada en el programa x)
+- b : Constante     (declarada en el programa x)
+- c : Constante     (declarada en el programa x)
+- d : Variable      (declarada en el programa x)
+- e : Variable      (declarada en el programa x)
+- f : Variable      (declarada en el programa x)
+- y : Procedimiento (declarado en el programa x)
+
+2. DENTRO DEL PROCEDIMIENTO y:
+--------------------------------------------------------------------
+- a : Constante     (declarada en el programa x)
+- b : Variable      (declarada en el procedimiento y; oculta a la constante b de x)
+- c : Constante     (declarada en el programa x)
+- d : Variable      (declarada en el programa x)
+- e : Variable      (declarada en el procedimiento y; oculta a la variable e de x)
+- f : Variable      (declarada en el programa x)
+- z : Procedimiento (declarado en el procedimiento y)
+- y : Procedimiento (declarado en el programa x, accesible por recursion)
+
+3. DENTRO DEL PROCEDIMIENTO z:
+--------------------------------------------------------------------
+- a : Constante     (declarada en el programa x)
+- b : Variable      (declarada en el procedimiento y)
+- c : Constante     (declarada en el programa x)
+- d : Variable      (declarada en el programa x)
+- e : Variable      (declarada en el procedimiento y)
+- f : Variable      (declarada en el procedimiento z; oculta a la variable f de x)
+- g : Variable      (declarada en el procedimiento z)
+- z : Procedimiento (declarado en el procedimiento y, accesible por recursion)
+- y : Procedimiento (declarado en el programa x)
+====================================================================
+}
